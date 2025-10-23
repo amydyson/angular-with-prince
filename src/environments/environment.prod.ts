601a817532396d9daf6e6683cabcea7404111620
amy <amybethdyson@gmail.com>
2025-10-22 18:30:29 -0700

    Update production environment to use working Heroku backend URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,4 @@
 export const environment = {
   production: true,
-  // TODO: Update this URL after deploying to Heroku
-  // Replace 'your-heroku-app-name' with your actual Heroku app name
-  apiUrl: 'https://your-heroku-app-name.herokuapp.com/api'
-  
-  // For reference, the old relative URL was:
-  // apiUrl: '/api' // This only works when backend and frontend are on same domain
+  apiUrl: 'https://angular-with-prince-backend-5de1af298c07.herokuapp.com/api'
 };
